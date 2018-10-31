814f609f5c351b9010fecc96511f9e6c9a9d6335
张博博 <zhangbobo1234yeah@126.com>
2018-10-31 19:53:35 +0800

    添加小知识点类库

---
+++ SomeMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SomeMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void AboutAttribute<T>() where T : new()
+        {
+            //排除含有NotMapped的字段
+            PropertyInfo[] propertis = new T().GetType().GetProperties().Where(v => v.GetCustomAttributes(typeof(NotMappedAttribute), true).Length == 0).ToArray();
+
+            PropertyInfo[] propertys = new T().GetType().GetProperties().Where(p => !Attribute.IsDefined(p, typeof(NotMappedAttribute))).ToArray();
+        }
+    }
+}
