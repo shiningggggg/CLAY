328721cfcf5b0191d318d5e0fa00da30e2a146d9
张博博 <zhangbobo1234yeah@126.com>
2019-02-12 19:52:52 +0800

    .

--- ThreadHelper/Program.cs
+++ ThreadHelper/Program.cs
@@ -10,7 +10,7 @@ namespace ThreadHelper
         static private void ThreadWork(object state)
         {
             int i = (int)state;
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 1; j++)
             {
                 Thread.Sleep(1000);
                 Console.WriteLine($"current thread {Thread.CurrentThread.ManagedThreadId} wait for {i * 10 + j + 1} seconds");
@@ -21,74 +21,82 @@ namespace ThreadHelper
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            #region 普通多线程
-            Ticket ticket = new Ticket();
-            Person[] person = new Person[10] {
-                new Person("Nicholas",21,"0000000000"),
-                new Person("Nate",38,"1111111111"),
-                new Person("Vincent",21,"2222222222"),
-                new Person("Nike",51,"3333333333"),
-                new Person("Gary",21,"0000000000"),
-                new Person("Charles",38,"1111111111"),
-                new Person("Karl",21,"2222222222"),
-                new Person("Katharine",51,"3333333333"),
-                new Person("Lee",21,"0000000000"),
-                new Person("Ann",38,"1111111111"),
-            };
-            ThreadMessage("MainThread start");
-            Console.WriteLine();
-            //定义一个线程组
-            Thread[] t = new Thread[person.Length];
-            //启动多个线程，每个线程执行相同的操作
-            for (int i = 0; i < person.Length; i++)
-            {
-                t[i] = new Thread(new ParameterizedThreadStart(BuyTicket));
-                t[i].Start(ticket);
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Main thread do work!");
-                Thread.Sleep(200);
-            }
-            #endregion
+            //#region 普通多线程
+            //Ticket ticket = new Ticket();
+            //Person[] person = new Person[10] {
+            //    new Person("Nicholas",21,"0000000000"),
+            //    new Person("Nate",38,"1111111111"),
+            //    new Person("Vincent",21,"2222222222"),
+            //    new Person("Nike",51,"3333333333"),
+            //    new Person("Gary",21,"0000000000"),
+            //    new Person("Charles",38,"1111111111"),
+            //    new Person("Karl",21,"2222222222"),
+            //    new Person("Katharine",51,"3333333333"),
+            //    new Person("Lee",21,"0000000000"),
+            //    new Person("Ann",38,"1111111111"),
+            //};
+            //ThreadMessage("MainThread start");
+            //Console.WriteLine();
+            ////定义一个线程组
+            //Thread[] t = new Thread[person.Length];
+            ////启动多个线程，每个线程执行相同的操作
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    t[i] = new Thread(new ParameterizedThreadStart(BuyTicket));
+            //    t[i].Start(ticket);
+            //}
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Main thread do work!");
+            //    Thread.Sleep(200);
+            //}
+            //#endregion
 
-            #region 线程池
-            Ticket poolticket = new Ticket();
-            Person[] poolperson = new Person[10]{
-                    new Person("Nicholas", 21, "000000000000000000"),
-                    new Person("Nate", 38, "111111111111111111"),
-                    new Person("Vincent", 21, "222222222222222222"),
-                    new Person("Niki", 51, "333333333333333333"),
-                    new Person("Gary", 28, "444444444444444444"),
-                    new Person("Charles", 49, "555555555555555555"),
-                    new Person("Karl ", 55, "666666666666666666"),
-                    new Person("Katharine", 19, "777777777777777777"),
-                    new Person("Lee", 25, "888888888888888888"),
-                    new Person("Ann", 34, "99999999999999999"),
-            };
-            ThreadPool.SetMaxThreads(1000, 1000);
-            ThreadPool.SetMinThreads(2, 2);
+            //#region 线程池
+            //Ticket poolticket = new Ticket();
+            //Person[] poolperson = new Person[10]{
+            //        new Person("Nicholas", 21, "000000000000000000"),
+            //        new Person("Nate", 38, "111111111111111111"),
+            //        new Person("Vincent", 21, "222222222222222222"),
+            //        new Person("Niki", 51, "333333333333333333"),
+            //        new Person("Gary", 28, "444444444444444444"),
+            //        new Person("Charles", 49, "555555555555555555"),
+            //        new Person("Karl ", 55, "666666666666666666"),
+            //        new Person("Katharine", 19, "777777777777777777"),
+            //        new Person("Lee", 25, "888888888888888888"),
+            //        new Person("Ann", 34, "99999999999999999"),
+            //};
+            //ThreadPool.SetMaxThreads(1000, 1000);
+            //ThreadPool.SetMinThreads(2, 2);
 
-            ThreadMessage("MainThread start");
-            Console.WriteLine();
-            foreach (Person p in poolperson)
+            //ThreadMessage("MainThread start");
+            //Console.WriteLine();
+            //foreach (Person p in poolperson)
+            //{
+            //    ThreadPool.QueueUserWorkItem(new WaitCallback(BuyTicket), ticket);
+            //}
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Main thread do work!");
+            //    Thread.Sleep(200);
+            //}
+            //#endregion
+            for (int k = 0; k < 3; k++)
             {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(BuyTicket), ticket);
+                #region 通过事件或者其他的内核对象来实现同步机制，返回线程状态；并检测最后一个完成的线程，来提高效率
+                for (int i = 0; i < _num; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(ThreadWork, i);
+                }
+                finish.WaitOne();
+                //finish.Close();
+                
+                //finish.Reset();如果不Reset的话，不会等待线程池线程执行完成。
+                Console.WriteLine("重置了");
+                _num = 10;
             }
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Main thread do work!");
-                Thread.Sleep(200);
-            }
-            #endregion
 
-            #region 通过事件或者其他的内核对象来实现同步机制，返回线程状态；并检测最后一个完成的线程，来提高效率
-            for (int i = 0; i < _num; i++)
-            {
-                ThreadPool.QueueUserWorkItem(ThreadWork, i);
-            }
-            finish.WaitOne();
-            finish.Close();
+
             #endregion
             Console.ReadKey();
         }
